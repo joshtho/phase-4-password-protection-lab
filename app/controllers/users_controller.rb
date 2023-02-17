76ce742d2c6441899ba6ae1149e93fae9f5a1280
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-02-17 09:07:26 -0600

    commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def show 
+        def show
+            user = User.find(session[:user_id])
+            render json: user
+        end
+    end
+
+    private 
+
+    def user_params
+        params.permit(:username, :password)
+    end
+end
